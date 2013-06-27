cd6c67fe10a72dce44df9172560c57d80db03204
Benjamin Fleischer <github@benjaminfleischer.com>
2013-06-27 12:40:47 -0500

    Add rake test from rails code base

---
+++ test/rake_test.rb
@@ -0,0 +1,15 @@
+# coding:utf-8
+require 'test_helper'
+require 'code_metrics/railtie'
+
+module CodeMetrics
+  class RakeTest < ActiveSupport::TestCase
+    def test_code_metrics_sanity
+      assert_match "Code LOC: 5     Test LOC: 0     Code to Test Ratio: 1:0.0",
+        Dir.chdir(app_path){ `rake stats` }
+    end
+    def app_path
+      File.expand_path('../dummy', __FILE__)
+    end
+  end
+end
