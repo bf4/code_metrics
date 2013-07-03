564d908a66cd142955e944cbfcce2feda859cb6f
ZPH <Zander@civet.ws>
2013-07-03 01:10:28 +0000

    BF/ZPH Test against rake code_metrics:stats instead of the rails task

--- test/rake_test.rb
+++ test/rake_test.rb
@@ -6,7 +6,7 @@ module CodeMetrics
   class RakeTest < ActiveSupport::TestCase
     def test_code_metrics_sanity
       assert_match "Code LOC: 5     Test LOC: 0     Code to Test Ratio: 1:0.0",
-        Dir.chdir(app_path){ `rake stats` }
+        Dir.chdir(app_path){ `rake code_metrics:stats` }
     end
     def app_path
       File.expand_path('../dummy', __FILE__)
