f093173e36ffbf5f89f7efb664b6a51e6fda5e70
Benjamin Fleischer <github@benjaminfleischer.com>
2013-07-05 15:08:37 -0500

    Ensure test rails app logger is silent

--- test/dummy/config/environments/test.rb
+++ test/dummy/config/environments/test.rb
@@ -33,4 +33,8 @@ Dummy::Application.configure do
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
+
+  config.log_level = :fatal
+  config.logger = Logger.new("/dev/null")
+
 end
